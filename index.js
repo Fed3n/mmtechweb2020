58e5f811cfb2a2f73e4b9c8e48bc0435d87df923
Andrea Ricciardelli <masterthunderman@gmail.com>
2020-09-03 16:33:33 +0200

    Aggiornamenti a player e server

--- index.js
+++ index.js
@@ -7,27 +7,52 @@ const fileUpload = require('express-fileupload')
 const host = "localhost";
 const port = 8080;
 
+var uid_generator = 0;
+
 app.use(fileUpload());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 app.get('/', (req, res) => {
-	return res.sendFile(path.join(__dirname + "/player.html"));
+    return res.sendFile(path.join(__dirname + "/player.html"));
 });
 
 app.get('/autore', (req, res) => {
-	return res.sendFile(path.join(__dirname + "/autore.html"));
+    return res.sendFile(path.join(__dirname + "/autore.html"));
 });
 
 app.get('/valutatore', (req, res) => {
     return res.sendFile(path.join(__dirname + "/valutatore.html"));
+});
+
+
+//## PLAYER ##//
+app.get('/uid', (req, res) => {
+    uid_generator++;
+    return res.send(uid_generator.toString());
+});
+app.get('/quest', (req, res) => {
+    console.log('Richiesta lista di files');
+    let load = fs.readdirSync('quest');
+    load.forEach(file => { console.log(file); });
+    return res.send(load);
+});
+
+app.get('/quest:questId', (req, res) => {
+    console.log(`Richiesto json ${req.params.questId}!`);
+    let load = fs.readFileSync(`quest/${req.params.questId}.json`);
+    let json = JSON.parse(load);
+    console.log(json);
+    res.setHeader('Content-Type','application/json');
+    console.log("Header set");
+    return res.json(json);
 });
 
 app.post('/players', (req, res) => {
@@ -45,84 +70,84 @@ app.post('/players', (req, res) => {
 //## AMBIENTE AUTORE ##//
 //Prende i file info.json delle storie in cui sono contenuti metadata per la rappresentazione (i.e. nome,accessibilità,etc)
 app.get('/stories', (req, res) => {
-	console.log("Getting content of ./story directory...")
-	let entrylist = fs.readdirSync(path.join(__dirname + "/story"));
-	load = [];
-	//Per ogni entry mando oggetto con info
-	for(entry of entrylist){
-		let info = fs.readFileSync(path.join(__dirname + "/story/" + entry + "/info.json"));
-		info = JSON.parse(info);
-		load.push(info);
-	}
-	res.send(load);
+    console.log("Getting content of ./story directory...")
+    let entrylist = fs.readdirSync(path.join(__dirname + "/story"));
+    load = [];
+    //Per ogni entry mando oggetto con info
+    for(entry of entrylist){
+        let info = fs.readFileSync(path.join(__dirname + "/story/" + entry + "/info.json"));
+        info = JSON.parse(info);
+        load.push(info);
+    }
+    res.send(load);
 });
 
 app.get('/stories/:storyName', (req, res) => {
-	var story = req.params.storyName;
-	var jsonpath = path.join(__dirname + "/story/" + story + "/" + story + ".json")
-	var metapath = path.join(__dirname + "/story/" + story + "/" + "info.json")
-	console.log("Getting story " + story);
-	let json = fs.readFileSync(jsonpath);
-	json = JSON.parse(json);
-	let meta = fs.readFileSync(metapath);
-	meta = JSON.parse(meta);
-	let load = {
-		json: json,
-		meta: meta
-	};
-	res.setHeader('Content-Type','application/json');
-	res.json(load);
+    var story = req.params.storyName;
+    var jsonpath = path.join(__dirname + "/story/" + story + "/" + story + ".json")
+    var metapath = path.join(__dirname + "/story/" + story + "/" + "info.json")
+    console.log("Getting story " + story);
+    let json = fs.readFileSync(jsonpath);
+    json = JSON.parse(json);
+    let meta = fs.readFileSync(metapath);
+    meta = JSON.parse(meta);
+    let load = {
+        json: json,
+        meta: meta
+    };
+    res.setHeader('Content-Type','application/json');
+    res.json(load);
 });
 
 app.post('/stories', (req, res) => {
-	console.log("Posting story :)");
-	var storyName = req.body.storyName;
-	var json = req.body.json;
-	var meta = req.body.meta;
-	var storydir = path.join(__dirname + "/story");
-	var newdir = path.join(storydir + "/" + storyName);
-	//Se la directory c'è già non la ricrea
-	fs.mkdirSync(newdir, { recursive: true });
-	fs.writeFile(path.join(newdir + "/" + storyName + ".json"), JSON.stringify(json), (error) => {
-		if(error) {
-			throw error;
-		}
-	});
-	fs.writeFile(path.join(newdir + "/" + "info.json"), JSON.stringify(meta), (error) => {
-		if(error) {
-			throw error;
-		}
-	});
-	res.send(":)");
+    console.log("Posting story :)");
+    var storyName = req.body.storyName;
+    var json = req.body.json;
+    var meta = req.body.meta;
+    var storydir = path.join(__dirname + "/story");
+    var newdir = path.join(storydir + "/" + storyName);
+    //Se la directory c'è già non la ricrea
+    fs.mkdirSync(newdir, { recursive: true });
+    fs.writeFile(path.join(newdir + "/" + storyName + ".json"), JSON.stringify(json), (error) => {
+        if(error) {
+            throw error;
+        }
+    });
+    fs.writeFile(path.join(newdir + "/" + "info.json"), JSON.stringify(meta), (error) => {
+        if(error) {
+            throw error;
+        }
+    });
+    res.send(":)");
 });
 
 app.delete('/stories', (req, res) => {
-	var story = req.query.storyName;
-	console.log("Deleting story: " + story);
-	var storydir = path.join(__dirname + "/story");
-	var dir = path.join(storydir + "/" + story);
-	fs.rmdir(dir, { recursive: true }, (error) => {
-		if(error) throw error;
-		console.log("Deleted story " + story);
-		res.send(":)");
-	});
+    var story = req.query.storyName;
+    console.log("Deleting story: " + story);
+    var storydir = path.join(__dirname + "/story");
+    var dir = path.join(storydir + "/" + story);
+    fs.rmdir(dir, { recursive: true }, (error) => {
+        if(error) throw error;
+        console.log("Deleted story " + story);
+        res.send(":)");
+    });
 });
 
 app.get('/stories/:storyName/images', (req, res) =>{
-	console.log(`Getting content of /story/${req.params.storyName}/images`);
-	var imgdir = path.join(__dirname + `/story/${req.params.storyName}/images/`);
-	let entrylist = fs.readdirSync(imgdir);
-	console.log(entrylist);
-	res.send(entrylist);
+    console.log(`Getting content of /story/${req.params.storyName}/images`);
+    var imgdir = path.join(__dirname + `/story/${req.params.storyName}/images/`);
+    let entrylist = fs.readdirSync(imgdir);
+    console.log(entrylist);
+    res.send(entrylist);
 });
 
 app.post('/stories/:storyName/images', (req, res) => {
-	var targetDir = path.join(__dirname + "/story/" + req.params.storyName + "/images/");
-	fs.mkdirSync(targetDir, { recursive: true });
-	fs.writeFile(path.join(targetDir + req.files.image.name), req.files.image.data, (error) => {
-		if(error)	throw error;
-	});
-	res.send(":)");
+    var targetDir = path.join(__dirname + "/story/" + req.params.storyName + "/images/");
+    fs.mkdirSync(targetDir, { recursive: true });
+    fs.writeFile(path.join(targetDir + req.files.image.name), req.files.image.data, (error) => {
+        if(error)   throw error;
+    });
+    res.send(":)");
 });
 
 
